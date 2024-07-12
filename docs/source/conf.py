92d07ed93e3d72415bbad87d42a5144bba600f95
Kristina Gagalova <kristina.gagalova@curtin.edu.au>
2024-07-12 15:47:14 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MycoProcessor'
+copyright = '2024, Curtin University, GRDC and CCDM'
+author = 'Kristina K. Gagalova & members at CCDM'
 
 release = '0.1'
 version = '0.1.0'
